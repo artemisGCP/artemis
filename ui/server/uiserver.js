4e89268f7cbe2dac7748c60f2abb31461b2967d5
jjbeck <jordan_becker@brown.edu>
2021-02-17 14:47:24 -0500

    UI scaffold

---
+++ ui/server/uiserver.js
@@ -0,0 +1,50 @@
+const express = require('express');
+require('dotenv').config();
+const proxy = require('http-proxy-middleware');
+const path = require('path');
+
+const port = process.env.UI_SERVER_PORT || 9000;
+const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT || 'http://localhost:3000/graphql';
+const env = { UI_API_ENDPOINT };
+const apiProxyTarget = process.env.API_PROXY_TARGET;
+
+const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
+
+const app = express();
+
+if (enableHMR && (process.env.NODE_ENV !== 'production')) {
+  console.log('Adding dev middleware, enabling HMR');
+  /* eslint "global-require": "off" */
+  /* eslint "import/no-extraneous-dependencies": "off" */
+
+  const webpack = require('webpack');
+  const devMiddleware = require('webpack-dev-middleware');
+  const hotMiddleware = require('webpack-hot-middleware');
+
+  const config = require('../webpack.config.js');
+  config.entry.app.push('webpack-hot-middleware/client');
+  config.plugins = config.plugins || [];
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+  const compiler = webpack(config);
+  app.use(devMiddleware(compiler));
+  app.use(hotMiddleware(compiler));
+}
+
+app.use(express.static('public'));
+
+if (apiProxyTarget) {
+  app.use('/graphql', proxy({ target: apiProxyTarget }));
+}
+
+app.get('/env.js', (req, res) => {
+  res.send(`window.ENV = ${JSON.stringify(env)}`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`UI started on port ${port}`);
+});
---
+++ ui/src/containers/footer/footer.container.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css';
+
+export default class FooterContainer extends React.Component {
+
+    render() {
+        return (
+            <div className="footer-wrapper">
+                <div>Copyright &copy;</div>
+            </div>
+        )
+    }
+}
