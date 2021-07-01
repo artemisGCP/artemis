53b46f111c108382964db7919ec0cd60ff9c5575
jjbeck <jordan_becker@brown.edu>
2021-07-01 19:04:25 -0400

    electron

--- artemis/ui/src/index.js
+++ artemis/ui/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 
 import Page from './Page.jsx';
